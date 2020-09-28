339f63b6d8698ae94d259f2d142475eeb4fc97bd
richardsipek827 <62510960+richardsipek827@users.noreply.github.com>
2020-09-28 16:31:15 +0200

    Add files via upload

---
+++ Základy OOP.php
@@ -0,0 +1,33 @@
+<?php
+
+class User {
+
+    public $gut;
+    public $dut;
+    public $cat;
+    public $mat;
+    public $dei;
+    
+    private $a;
+    private $b;
+    
+    protected $qec;
+    protected $deck;
+    protected $cef;
+    
+    private function __construct() {
+        
+        
+        
+    }
+        
+    public function getNazevAtrubutuPrefix() {
+ }
+  
+  
+ 
+ 
+    
+    
+  }
+    
